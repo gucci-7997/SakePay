c142acecbda64d864fddf4175e95e5587fdaea14
gucci-7997 <132871599+gucci-7997@users.noreply.github.com>
2025-02-21 21:36:56 +0900

    Update warikan.js

--- software/web/warikan.js
+++ software/web/warikan.js
@@ -1,7 +1,5 @@
-// https://script.google.com/macros/s/AKfycbzPY2-duPx8ulvGl_dih_2Bd6YkCM-_1mYczR9Kt_mdMP98EOxXuqXPVz9jad6kngLD/exec
-// https://script.google.com/macros/s/AKfycbxKSF9JKvL_SDS5dTjkAMfCU733gTm8JErSNHKzQyThQhUNWuIv6uCZLN05rsIg8tKD/exec
 
-fetch('https://script.google.com/macros/s/AKfycbxKSF9JKvL_SDS5dTjkAMfCU733gTm8JErSNHKzQyThQhUNWuIv6uCZLN05rsIg8tKD/exec')
+fetch('URL')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.log("error: ", error));
